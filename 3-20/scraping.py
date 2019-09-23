c17637f63a8b4b2b013419f3f1c7f867aea65bf0
平野元規 <hiranomotoki@hiranomotokinoMacBook-Pro.local>
2019-09-24 04:38:12 +0900

    add scraping.py,index.html

---
+++ 3-20/scraping.py
@@ -0,0 +1,27 @@
+import urllib.request
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self,site):
+        self.site = site
+        self.urls = set()
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        sp = BeautifulSoup(html,'html.parser')
+        for tag in sp.find_all('a'):
+            url = tag.get('href')
+            if url is None:
+                continue
+            if not url.endswith('.html'):
+                continue
+            if url in self.urls:
+                continue
+            self.urls.add(url)
+            full_url = urljoin(self.site,url)      
+            print('\n' + full_url)
+
+news = 'https://trendy.nikkeibp.co.jp/news/'
+Scraper(news).scrape()
